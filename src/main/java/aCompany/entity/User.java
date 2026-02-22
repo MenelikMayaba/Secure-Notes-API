e63c529df8950b62a8010f1b88699628ea0cbb8e
MenelikMulima <mayabamulima147@gmail.com>
2026-02-22 23:25:02 +0200

    User entity class

---
+++ src/main/java/aCompany/entity/User.java
@@ -0,0 +1,4 @@
+package aCompany.entity;
+
+public class User {
+}
--- src/test/java/aCompany/UserTest.java
+++ src/test/java/aCompany/UserTest.java
@@ -1,4 +1,4 @@
-package com.example.securitydemo.entity;
+package aCompany.entity;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
