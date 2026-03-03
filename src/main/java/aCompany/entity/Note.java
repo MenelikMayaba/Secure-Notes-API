140f7b0bb97a1f59e094d555d579cb99e876b1f6
MenelikMulima <mayabamulima147@gmail.com>
2026-03-03 09:56:07 +0200

    Note Class

--- src/main/java/aCompany/entity/Note.java
+++ src/main/java/aCompany/entity/Note.java
@@ -1,4 +1,47 @@
-package aCompany;
+package aCompany.entity;
 
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "notes")
 public class Note {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    User user;
+    String title;
+    String content;
+
+    public Note(){}
+
+    public Note(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
