aeb067e9e42f38c46984a8adbeabd6ba85aa74f8
MenelikMulima <mayabamulima147@gmail.com>
2026-03-03 09:57:16 +0200

    created method to find user by username

--- src/main/java/aCompany/Repository/UserRepository.java
+++ src/main/java/aCompany/Repository/UserRepository.java
@@ -1,4 +1,12 @@
 package aCompany.Repository;
 
-public class UserRepository {
+import aCompany.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findByUsername(String Username);
+
 }
