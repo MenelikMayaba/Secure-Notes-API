dc7369e79f0fec34015eb10e6a84a6820cd1ded7
MenelikMulima <mayabamulima147@gmail.com>
2026-03-06 08:37:21 +0200

    encoded password to make the registerShouldEncodePasswordAndSaveUser test pass in the user service test

--- src/main/java/aCompany/Service/UserService.java
+++ src/main/java/aCompany/Service/UserService.java
@@ -1,4 +1,33 @@
-package aCompany;
+package aCompany.Service;
 
+import aCompany.Repository.UserRepository;
+import aCompany.entity.User;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public User register(User user){
+        User newUser = new User();
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        newUser.setPassword(encodedPassword);
+        newUser.setUsername(String.valueOf(user.getUsername()));
+        return userRepository.save(newUser);
+
+    }
 }
+
