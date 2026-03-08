4c202452ed087eb22166b768017294f33e4484c4
MenelikMulima <mayabamulima147@gmail.com>
2026-03-08 08:26:55 +0200

    created getters, setters, and contstructor for noteService

--- src/main/java/aCompany/Service/NoteService.java
+++ src/main/java/aCompany/Service/NoteService.java
@@ -1,4 +1,43 @@
 package aCompany.Service;
 
+import aCompany.entity.User;
+
 public class NoteService {
+
+    private String title;
+    private String content;
+    private User user;
+
+    public NoteService() {
+    }
+
+    public NoteService(String title, User user, String content) {
+        this.title = title;
+        this.user = user;
+        this.content = content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
