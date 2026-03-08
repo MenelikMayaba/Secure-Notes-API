2a6fb9eb39477f779c8d2c2d64d72b7897a496d6
MenelikMulima <mayabamulima147@gmail.com>
2026-03-08 18:12:32 +0200

    made note repository an interface that extends the JpaRepo

--- src/main/java/aCompany/Repository/NoteRepository.java
+++ src/main/java/aCompany/Repository/NoteRepository.java
@@ -1,4 +1,11 @@
 package aCompany.Repository;
 
-public class NoteRepository {
+import aCompany.entity.Note;
+import aCompany.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface NoteRepository extends JpaRepository<Note, Long> {
+
 }
