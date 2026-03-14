002a51666ba584f91787ccf8662d49b7b1bfeac9
MenelikMulima <mayabamulima147@gmail.com>
2026-03-14 07:46:21 +0200

    removed redundant setter for username

--- src/main/java/aCompany/Controller/UserController.java
+++ src/main/java/aCompany/Controller/UserController.java
@@ -1,4 +1,36 @@
 package aCompany.Controller;
 
+
+import aCompany.Repository.UserRepository;
+import aCompany.Service.UserService;
+import aCompany.entity.Roles;
+import aCompany.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    @PostMapping
+    public ResponseEntity<User> register(User user) {
+        String encodedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(encodedPassword);
+        user.setRole(Roles.USER);
+        userService.register(user);
+        return ResponseEntity.ok(user);
+
+
+    }
 }
