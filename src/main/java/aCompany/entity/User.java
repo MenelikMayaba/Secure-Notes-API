65e70afe28915b6c2059908dc9bd7ae7ebcee661
MenelikMayaba <mayabamulima147@gmail.com>
2026-04-16 13:29:54 +0200

    Comment out getter and setter methods for User
    Comment out user-related methods in User.java

--- src/main/java/aCompany/entity/User.java
+++ src/main/java/aCompany/entity/User.java
@@ -30,32 +30,32 @@ public class User {
     }
 
 
-    public String getUsername() {
-        return username;
-    }
+    // public String getUsername() {
+    //     return username;
+    // }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+    // public void setUsername(String username) {
+    //     this.username = username;
+    // }
 
-    public String getPassword() {
-        return password;
-    }
+    // public String getPassword() {
+    //     return password;
+    // }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+    // public void setPassword(String password) {
+    //     this.password = password;
+    // }
 
-    public Roles getRole() {
-        return role;
-    }
+    // public Roles getRole() {
+    //     return role;
+    // }
 
-    public void setRole(Roles role) {
-        this.role = role;
-    }
+    // public void setRole(Roles role) {
+    //     this.role = role;
+    // }
 
-    public Object getTitle() {
-        return null;
+    // public Object getTitle() {
+    //     return null;
 
-    }
+    // }
 }
